798043eb9036d57f811ce716910b143e776c97df
Nicolas Largueze <nick@nicklabs.io>
2023-05-15 15:51:10 +0200

    feat: added export to type macros

--- src/schema/types.rs
+++ src/schema/types.rs
@@ -24,6 +24,7 @@ pub trait DbValue: Debug {
 }
 
 /// Implements the nullable variant of a db type
+#[macro_export]
 macro_rules! impl_db_nullable_variant {
     ($TY:ty, $DB_TY:tt) => {
         impl DbType for Option<$TY> {
@@ -50,6 +51,7 @@ macro_rules! impl_db_nullable_variant {
 }
 
 /// Implements the array variant of a db type
+#[macro_export]
 macro_rules! impl_db_array_variant {
     ($TY:ty, $DB_TY:tt) => {
         impl DbType for Vec<$TY> {
@@ -89,6 +91,7 @@ macro_rules! impl_db_array_variant {
 }
 
 /// Implements the map variant of a db type
+#[macro_export]
 macro_rules! impl_db_map_variant {
     ($TY:ty, $DB_TY:tt) => {
         impl DbType for HashMap<String, $TY> {
@@ -141,6 +144,7 @@ macro_rules! impl_db_map_variant {
 }
 
 /// Implements a base type which implements `to_string()` and `FromStr`
+#[macro_export]
 macro_rules! impl_db_base_type {
     ($TY:ty, $DB_TY:tt) => {
         impl DbType for $TY {
